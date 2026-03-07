5e36861416bbd21b2e4088ab7b5c10108c9b11a3
Santosh Grampurohit <santoshvg@iisc.ac.in>
2026-03-07 15:11:56 +0530

    Updated sample test to use draw

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,11 +1,20 @@
-# contents of example.py
 from hypothesis import given, strategies as st
 
-@given(st.integers())
-def test_integers(n):
-    print(f"called with {n}")
-    result = n / 2
-    print(f"result is {result}")
-    assert n >= 0, "n must be non-negative"
 
-test_integers()
+@given(st.integers(min_value=0, max_value=10))
+def test_single_draw(n):
+    print(f"n = {n}")
+    assert 0 <= n <= 10
+
+# Using draw
+@st.composite
+def list_of_n_integers(draw):
+    n = draw(st.integers(min_value=1, max_value=5))
+    print(f"drew n = {n}")
+
+    return n
+
+
+@given(list_of_n_integers())
+def test_list_length_matches_n(n):
+    assert 1 <= n <= 5
